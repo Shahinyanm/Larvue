90d978229826e46666eb082a9c48f1518537a82c
unknown <shahinyanm@gmail.com>
2019-07-16 16:17:11 +0400

    Add Delete function for questions

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -58,7 +58,7 @@ class QuestionsController extends Controller
      */
     public function show(Question $question)
     {
-        //
+        return view('questions.show', compact('question'));
     }
 
     /**
@@ -94,6 +94,9 @@ class QuestionsController extends Controller
      */
     public function destroy(Question $question)
     {
-        //
+        $question->delete();
+
+        return redirect()->route('questions.index')->with('success','Your question has been deleted');
+
     }
 }
--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -33,7 +33,12 @@
                             <div class="d-flex align-items-center">
                                 <h3 class="mt-0"><a href="{{$question->url}}"> {{$question->title}} </a></h3>
                                 <div class="ml-auto">
-                                    <a href="{{route('questions.edit',$question->id)}}" class="btn bnt-sm btn-outline-info">Edit</a>
+                                    <a href="{{route('questions.edit',$question->id)}}" class="btn btn-sm btn-outline-info">Edit</a>
+                                    <form action="{{route('questions.destroy',$question->id)}}" method="POST" class="form-delete">
+                                        @method('DELETE')
+                                        @csrf
+                                        <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')">Delete</button>
+                                    </form>
                                 </div>
                             </div>
                                     <p class="lead">
