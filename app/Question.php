1d592acd58552cce80e6834c8bee1da20f04a6cd
unknown <shahinyanm@gmail.com>
2019-07-22 12:17:43 +0400

    Add Votable trait to Question and Answer Model

--- app/Question.php
+++ app/Question.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
+    use VotableTrait;
+
     public $timestamps = false;
     protected $fillable = ['title', 'body'];
 
@@ -81,17 +83,6 @@ class Question extends Model
         return $this->favorites->count();
     }
 
-    public function votes()
-    {
-        return $this->morphToMany(User::class,'votable');
-    }
 
-    public function upVotes(){
-        return $this->votes()->wherePivot('vote',1);
-    }
-
-    public function downVotes(){
-        return $this->votes()->wherePivot('vote',-1);
-    }
 
 }
